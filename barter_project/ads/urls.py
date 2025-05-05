5ded9a23f4564bc4aba57d7dac295139fdf6775a
ase1lo <vosheb556wow@gmail.com>
2025-05-05 21:42:24 +0300

    crud and html

--- barter_project/ads/urls.py
+++ barter_project/ads/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.ads_list, name='ads_list'),
     path('create/', views.create_ad, name='create_ad'),
-    path('success/', views.ad_success, name='ad_success')
+    path('success/', views.ad_success, name='ad_success'),
+    path('ads/<int:ad_id>/edit/', views.update_ad, name='update_ad'),
+    path('ads/<int:ad_id>/delete/', views.delete_ad, name='delete_ad'),
 ]
--- barter_project/ads/views.py
+++ barter_project/ads/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import AdForm
 from django.contrib.auth.decorators import login_required
+from .models import Ad
+
 
 # Create your views here.
 
 def home(request):
     return render(request, 'ads/home.html')
+
+def ads_list(request):
+    ads = Ad.objects.all().order_by('-created_at')
+    return render(request, 'ads/ads_list.html', {'ads': ads})
 
 @login_required
 def create_ad(request):
@@ -21,6 +27,29 @@ def create_ad(request):
     
     return render(request, 'ads/create_ad.html', {'form': form})
 
+def update_ad(request, ad_id):
+    ad = get_object_or_404(Ad, id=ad_id)
+    if ad.user != request.user:
+        return render(request, 'ads/permission_denied.html')
+    if request.method == 'POST':
+        form = AdForm(request.POST, instance=ad)
+        if form.is_valid():
+            form.save()
+            return redirect('ads_list')
+    else:
+        form = AdForm(instance=ad)
+    return render(request, 'ads/update_ad.html', {'form': form})
+
+def delete_ad(request, ad_id):
+    ad = get_object_or_404(Ad, id=ad_id)
+    if ad.user != request.user:
+        return render(request, 'ads/permission_denied.html')
+    
+    if request.method == 'POST':
+        ad.delete()
+        return redirect('ads_list')
+    
+    return render(request, 'ads/delete_ad.html', {'ad': ad})
 
 def ad_success(request):
     return render(request, 'ads/success.html')
